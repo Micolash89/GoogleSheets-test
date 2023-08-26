fb037e4aab7c4657e933a085cb194d53b2560403
javier <espindolajavier2013@gmail.com>
2023-08-26 17:08:29 -0300

    se agrego el boton eliminar de manera exitosa, elimina y muestra la tabla actualizada

--- js/buscar.js
+++ js/buscar.js
@@ -5,68 +5,66 @@
 let form = document.querySelector('form');
 let buscar = document.getElementById('palabra');
 let tbody = document.querySelector('tbody');
+let btn = document.getElementById('buscar');
 
+form.addEventListener('submit', find);
 
-
-form.addEventListener('submit', async function (e) {
+async function find(e) {
     e.preventDefault();
 
-    let btn = document.getElementById('buscar');
     btn.innerText = "buscando...";
 
     let titulo = document.getElementById('titulo');
-   let mensaje = document.getElementById("mensaje");
+    let mensaje = document.getElementById("mensaje");
+
+    actualizarTabla();
+};
+
+window.editar = async function editar(id) {
+try {
+    fetch("https://script.google.com/macros/s/AKfycbyLXj2BzeJ-fkwVZYcqrt5mzhVT7CkcYj3BeZEueIxcRscUbZJkuuzPxMasxqS92bJ1/exec" + `?update=true&id=${id}$data=${form[0].value}`)
+        .then(res => res.text())
+        .then(data => {
+            actualizarTabla();
+            console.log(data)});
+    }catch(error){
+        console.log(error);
+    }
+}
+window.eliminar = async function eliminar(id) {
+try {
+    fetch("https://script.google.com/macros/s/AKfycbyLXj2BzeJ-fkwVZYcqrt5mzhVT7CkcYj3BeZEueIxcRscUbZJkuuzPxMasxqS92bJ1/exec" + `?del=true&id=${id}`)
+        .then(res => res.text())
+        .then(data => {
+            actualizarTabla();
+            console.log(data)});
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function actualizarTabla() {
     try {
         fetch("https://script.google.com/macros/s/AKfycbwd1qG3_DZ0Dv0ytrkOeXdOQ2-p2R5QYQwkImHGdXEjAGcR06d7G7EJfilRLcBDG9D2/exec")
             .then(res => res.json())
             .then(data => {
                 const vec = data.nombre;
                 if (vec.length) {
-                    let text = vec.map((j) => {
+                    const text = vec.map((j) => {
                         return `
                     <tr>
                         <td>${j[0]}</td>
                         <td>${j[1]}</td>
                         <td>${j[2]}</td>
-                         <td>${j[3]}</td>
-                        <td class="editar" onClick="editar">Editar</td>
-                        <td class="eliminar" onClick="eliminar"  >eliminar</td>
+                        <td>${j[3]}</td>
+                        <td class="editar" onClick="editar(${j[0]})">Editar</td>
+                        <td class="eliminar" onClick="eliminar(${j[0]})">eliminar</td>
                     </tr>
                  `});
                     tbody.innerHTML = text.join("");
-                    // let edit = document.querySelectorAll('.editar');
-                    // console.log(edit);
-                    // console.log($(td).closest('tr').children().eq(0).text());
-                    // function editar(e) {
-                    // console.log(e.target)
-                    // let id = $(e.target).closest('tr').children().eq(0).text();
-                    // fetch(API + `?del=true&id=${id}`).then(res => res.text()).then(data => console.log(data));
-
-
-                    // edit.forEach(function (td) {
-                    //let id = $(td).closest('tr').children().eq(0).text();
-                    // td.addEventListener('click', async () => {
-                    //     fetch(api + `?del=true&id=${id}`).then(res => res.text()).then(data => console.log(data));
-                    // });
-                    // });
-                    //}
-
-                    function eliminar(td) {
-                        let eli = document.querySelectorAll('.eliminar');
-                        eli.forEach(function (td) {
-                            let id = $(td).closest('tr').children().eq(0).text();
-                            td.addEventListener('click', function () {
-
-                            });
-                        });
-                    }
-
-
                 }
                 else
                     mensaje.innerHTML = "no hay datos";
-
-
 
             });
     } catch (error) {
@@ -74,6 +72,4 @@ form.addEventListener('submit', async function (e) {
     } finally {
         btn.innerText = "Buscar";
     }
-
-})
-
+}
